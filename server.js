bd28e556b8045ca427e0f0c9eea65222bf29e057
Thibault_Merelle <merellethibault@gmail.com>
2021-04-21 10:00:22 +0200

    adding psql query using pg

--- server.js
+++ server.js
@@ -1,15 +1,61 @@
 const express = require("express")
+const {Client} = require("pg")
 const app = express()
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.get("/", (req, res) => {
-    res.json({"message" : "Online !!"})
+    res.json({"message" : "Online on VM1 !!"})
 })
 
-app.get("/table", (req, res) => {
-    res.json({"message" : "Table query page !"})
-})
+
+const client = new Client({
+    user: "postgres",
+    password: "azureuser",
+    host: "10.7.0.8",
+    database: "postgres",
+    port: 5432,
+  });
+  
+client.connect();
+  
+  app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+    );
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+  });
+  
+
+  app.get("/table", (req, res) => {
+    const query = `SELECT * FROM moto`;
+    client.query(query, (err, result) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.json(result)
+      client.end();
+    });
+  })
+  
+//   app.get("/sum", (req, res)=> {
+//       request.query(
+//         "SELECT SUM(price) as sum FROM [moto].[postgres]",
+//         (err, result) => {
+//           console.log(err, result);
+//           const totale = Math.round((result.recordset[0].total)*100)/100
+//           const rep = `${totale} euros`;
+//           res.send(rep);
+//         }
+//       );
+//       // res.json({id : "salut"})
+      
+//   })
+
+
 
 app.listen(port, () => {
-  console.log(Example app listening at http://localhost:${port});
+  console.log(`Example app listening at http://localhost:${port}`);
 });
